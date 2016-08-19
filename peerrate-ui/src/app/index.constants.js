80edf36ea4e5310b9536d8f30acd0248608f2a44
Jason Pierce <jason.pierce.designs@gmail.com>
2016-08-19 11:28:46 -0400

    [DHSC-25] adding initial UI scaffolding

---
+++ peerrate-ui/src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('dhsChallenge')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
