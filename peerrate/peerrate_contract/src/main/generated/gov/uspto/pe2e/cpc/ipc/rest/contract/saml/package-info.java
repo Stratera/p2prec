c49d9898d316e166609dc3c13ed8a757061846f4
Matt Young <myoung@strateratech.com>
2016-08-23 10:10:51 -0400

    cleaning up git ignore to ignore generated java dirs

--- peerrate/peerrate_contract/src/main/generated/gov/uspto/pe2e/cpc/ipc/rest/contract/saml/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://rest.ipc.cpc.pe2e.uspto.gov/contract/saml", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package gov.uspto.pe2e.cpc.ipc.rest.contract.saml;
