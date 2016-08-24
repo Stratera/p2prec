dae38e64c1618686597d0a85a5a37a0264655c79
Jason Pierce <jason.pierce.designs@gmail.com>
2016-08-23 22:10:40 -0400

    [DHSC-25] reverting to previous state and getting rid of useless add-ons that the scaffolding tool added to the project by default

--- peerrate-ui/src/app/index.route.js
+++ peerrate-ui/src/app/index.route.js
@@ -2,24 +2,18 @@
   'use strict';
 
   angular
-    .module('dhsChallenge',['view1'])
+    .module('dhsChallenge')
     .config(routerConfig);
 
   /** @ngInject */
   function routerConfig($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/');
     $stateProvider
       .state('home', {
         url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
-      })
-      .state('view1', {
-        url: '/view1',
-        templateUrl: 'app/view1/view1.html',
-        controller: 'View1Controller',
-        controllerAs: 'view1Ctrl'
       });
+    $urlRouterProvider.otherwise('/');
   }
 })();
