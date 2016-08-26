b1a4c2e38d9b32d94cd0fc0736b764e82c7c5a94
ivaddiparti <indupadma@gmail.com>
2016-08-26 00:20:42 -0400

    added files under restmod

--- peerrate-ui/src/app/services/restmod/BaseModel.js
+++ peerrate-ui/src/app/services/restmod/BaseModel.js
@@ -1,13 +1,47 @@
-angular.module("services.restmod.BaseModel", [])
+/**
+ * @ngdoc module
+ * @name common.restmod.BaseModel
+ */
+angular.module("services.restmod.BaseModel", [ ])
 
+/**
+ * @ngdoc type
+ * @name BaseModel
+ * @description
+ * A place for global model enhancements
+ */
 .factory("BaseModel", function (restmod, servicesConfig) {
     var servicePrefix = servicesConfig.prefix;
     return restmod.mixin ({
         $config: {
-            style: "DHS",
+            style: "CPC",
             urlPrefix: servicePrefix,
             primaryKey: "id"
         },
-        
-    })
-})
+        $extend: {
+
+            Model: {
+                unpack: function (self, _raw) {
+                    return _raw.list || _raw;
+                }
+            },
+
+            Record: {
+                $extend: function (_other) {
+                    this.$dispatch("before-extend", _other);
+                    var value = this.$super(_other);
+                    this.$dispatch("after-extend", _other);
+                    return value;
+                }
+            }
+
+        },
+        $hooks: {
+            'before-request': function (req) {
+                if ((new this.$type()).useMock) {
+                    req.url = req.url.replace(/https.*cpcipcrestweb/, "/mock");
+                }
+            }
+        }
+    });
+});
