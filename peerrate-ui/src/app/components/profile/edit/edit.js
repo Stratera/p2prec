eafd8325ffcce0ada99dd18933500103c5d9c620
Jason Pierce <jason.pierce.designs@gmail.com>
2016-08-30 23:25:37 -0400

    adding angular form validation and binding

--- peerrate-ui/src/app/components/profile/edit/edit.js
+++ peerrate-ui/src/app/components/profile/edit/edit.js
@@ -18,8 +18,61 @@ angular.module("app.components.profile.edit", [
         })
     })
 
-    .controller("EditProfileController", EditProfileController);
+    .controller("EditProfileController", EditProfileController)
+
+    // .controller("EditFormController" , function ($rootScope, $scope) {
+    //     var vm = this;
+
+
+    //     $scope.$watch("vm.editProfileForm.firstName", function (newVal, oldVal) {
+    //         if (newVal >= 1) {
+    //             vm.firstName.$setValidity("firstName entered", true);
+    //         } else {
+    //             vm.firstName.$setValidity("firstName entered", false);
+    //         }
+    //     });
+    // })
 
 function EditProfileController($scope) {
+    var ctx = this;
+    // this.editProfileForm;
+    this.user = {
+        firstName:              "",
+        lastName:               "",
+        middleName:             "",
+        department:             "",
+        city:                   "",
+        state:                  "",
+        phone:                  "",
+        email:                  "",
+        workAnniversaryDate:    {
+            month:  "",
+            day:    "",
+            year:   ""
+        },
+        birthday:    {
+            month:  "",
+            day:    "",
+            year:   ""
+        }
+    };
+
+    $scope.$watch("this.user.firstName", function (newVal, oldVal) {
+        var isValid = newVal >= 1 ? true : false;
+        ctx.user.firstName.$setValidity("firstName", isValid);
+    });
+    
+    // $scope.watch("this.user.firstName", function () {
+    //     this.user.firstName.length >= 1 ? this.editProfileForm : true;
+    // });
+
 
 }
+
+EditProfileController.prototype = {
+    submitForm: function () {
+        // REST endpoint edit? user
+    },
+
+
+};
