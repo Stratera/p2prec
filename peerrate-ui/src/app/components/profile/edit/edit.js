3a9cfe946ef97ba0e07f37ebeb50348a8ca76b5d
ivaddiparti <indupadma@gmail.com>
2016-08-31 00:48:10 -0400

    WIP - added angular form elements

--- peerrate-ui/src/app/components/profile/edit/edit.js
+++ peerrate-ui/src/app/components/profile/edit/edit.js
@@ -1,78 +1,78 @@
 angular.module("app.components.profile.edit", [
-    "services.component",
-    "models.user"
+  "services.component",
+  "models.user"
 ])
 
-    .config(function (componentProvider) {
-        componentProvider.register({
-            name: 'editProfile',
-            title: 'Edit Profile',
-            controller: 'EditProfileController',
-            controllerAs: 'editProfileController',
-            templateUrl: 'app/components/profile/edit/edit.tpl.html',
-            access: "",
-            frontpage: true,
-            scope: true,
-            reloadOnSearch: true,
-            queryParams: []
-        })
+  .config(function (componentProvider) {
+    componentProvider.register({
+      name: 'editProfile',
+      title: 'Edit Profile',
+      controller: 'EditProfileController',
+      controllerAs: 'editProfileController',
+      templateUrl: 'app/components/profile/edit/edit.tpl.html',
+      access: "",
+      frontpage: true,
+      scope: true,
+      reloadOnSearch: true,
+      queryParams: []
     })
+  })
 
-    .controller("EditProfileController", EditProfileController)
+  .controller("EditProfileController", EditProfileController);
 
-    // .controller("EditFormController" , function ($rootScope, $scope) {
-    //     var vm = this;
+// .controller("EditFormController" , function ($rootScope, $scope) {
+//     var vm = this;
 
 
-    //     $scope.$watch("vm.editProfileForm.firstName", function (newVal, oldVal) {
-    //         if (newVal >= 1) {
-    //             vm.firstName.$setValidity("firstName entered", true);
-    //         } else {
-    //             vm.firstName.$setValidity("firstName entered", false);
-    //         }
-    //     });
-    // })
+//     $scope.$watch("vm.editProfileForm.firstName", function (newVal, oldVal) {
+//         if (newVal >= 1) {
+//             vm.firstName.$setValidity("firstName entered", true);
+//         } else {
+//             vm.firstName.$setValidity("firstName entered", false);
+//         }
+//     });
+// })
 
 function EditProfileController($scope) {
-    var ctx = this;
-    // this.editProfileForm;
-    this.user = {
-        firstName:              "",
-        lastName:               "",
-        middleName:             "",
-        department:             "",
-        city:                   "",
-        state:                  "",
-        phone:                  "",
-        email:                  "",
-        workAnniversaryDate:    {
-            month:  "",
-            day:    "",
-            year:   ""
-        },
-        birthday:    {
-            month:  "",
-            day:    "",
-            year:   ""
-        }
-    };
+  var ctx = this;
+  // this.editProfileForm;
+  ctx.user = {
+    firstName:              "",
+    lastName:               "",
+    middleName:             "",
+    department:             "",
+    city:                   "",
+    state:                  "",
+    phone:                  "",
+    email:                  "",
+    workAnniversaryDate:    {
+      month:  "",
+      day:    "",
+      year:   ""
+    },
+    birthday:    {
+      month:  "",
+      day:    "",
+      year:   ""
+    }
+  };
 
-    $scope.$watch("this.user.firstName", function (newVal, oldVal) {
-        var isValid = newVal >= 1 ? true : false;
-        ctx.user.firstName.$setValidity("firstName", isValid);
-    });
-    
-    // $scope.watch("this.user.firstName", function () {
-    //     this.user.firstName.length >= 1 ? this.editProfileForm : true;
-    // });
+  $scope.$watch("this.user.firstName", function (newVal, oldVal) {
+    var isValid = newVal >= 1 ? true : false;
+    ctx.user.firstName.$setValidity("firstName", isValid);
+  });
+
+  // $scope.watch("this.user.firstName", function () {
+  //     this.user.firstName.length >= 1 ? this.editProfileForm : true;
+  // });
 
 
 }
 
 EditProfileController.prototype = {
-    submitForm: function () {
-        // REST endpoint edit? user
-    },
+  submitForm: function () {
+    // REST endpoint edit? user
+  }
 
 
 };
